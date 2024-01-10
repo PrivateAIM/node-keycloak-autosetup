1453e2cb22a08cfe3fdec56678687c84bca224a5
Maximilian Jugl <Maximilian.Jugl@medizin.uni-leipzig.de>
2024-01-10 09:51:55 +0100

    feat: test connection to Keycloak with direct access grant

--- project/main.py
+++ project/main.py
@@ -1,7 +1,31 @@
-def add(x: int, y: int):
-    return x + y
+import click
+from keycloak import KeycloakOpenIDConnection, KeycloakAdmin
+
+
+@click.command()
+@click.option("--kc-server-url", default="http://localhost:8080/")
+@click.option("--kc-master-realm-name", default="master")
+@click.option("--verify/--no-verify", default=True)
+@click.argument("kc_admin_username")
+@click.argument("kc_admin_password")
+def setup_keycloak(
+    kc_admin_username: str,
+    kc_admin_password: str,
+    kc_server_url: str,
+    kc_master_realm_name: str,
+    verify: bool,
+) -> None:
+    kc_oid = KeycloakOpenIDConnection(
+        server_url=kc_server_url,
+        username=kc_admin_username,
+        password=kc_admin_password,
+        realm_name=kc_master_realm_name,
+        verify=verify,
+    )
+
+    kc_admin = KeycloakAdmin(connection=kc_oid)
+    print(kc_admin.get_realms())
 
 
 if __name__ == "__main__":
-    print("Hello, PrivateAIM!")
-    print("Did you know that 2 + 3 =", add(2, 3))
+    setup_keycloak()
