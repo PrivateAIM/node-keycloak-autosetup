1d6de3b23b2475b19d7e849984a3d4cd5fe9f165
Maximilian Jugl <Maximilian.Jugl@medizin.uni-leipzig.de>
2024-01-10 13:57:24 +0100

    feat: add client creation

--- project/main.py
+++ project/main.py
@@ -12,10 +12,12 @@ from keycloak import KeycloakOpenIDConnection, KeycloakAdmin
 @click.argument("kc_admin_username")
 @click.argument("kc_admin_password")
 @click.argument("kc_realm_file")
+@click.argument("kc_client_file")
 def setup_keycloak(
     kc_admin_username: str,
     kc_admin_password: str,
     kc_realm_file: str,
+    kc_client_file: str,
     kc_server_url: str,
     kc_master_realm_name: str,
     verify: bool,
@@ -39,7 +41,7 @@ def setup_keycloak(
 
     realm_id = realm_payload["id"]
     realm_name = realm_payload["realm"]
-    logger.info('Read realm "%s" (%s) payload', realm_name, realm_id)
+    logger.info("Read realm `%s` (%s) payload", realm_name, realm_id)
 
     # create realm and perform sanity check
     kc_admin.create_realm(realm_payload, skip_exists=True)
@@ -49,6 +51,26 @@ def setup_keycloak(
     assert kc_realm["realm"] == realm_name
 
     logger.info("Realm successfully created")
+
+    # switch realms
+    kc_admin.change_current_realm(realm_name)
+
+    # load client payload
+    with open(kc_client_file, mode="r", encoding="utf-8") as f:
+        client_payload = json.load(f)
+
+    kc_client_id = client_payload["clientId"]
+    kc_client_secret = client_payload["secret"]
+    logger.info("Read client `%s`", kc_client_id)
+
+    # create client and perform sanity check
+    kc_client_uuid = kc_admin.create_client(client_payload, skip_exists=True)
+    kc_client = kc_admin.get_client(kc_client_uuid)
+
+    assert kc_client["clientId"] == kc_client_id
+    assert kc_client["secret"] == kc_client_secret
+
+    logger.info("Client successfully created")
 
 
 if __name__ == "__main__":
